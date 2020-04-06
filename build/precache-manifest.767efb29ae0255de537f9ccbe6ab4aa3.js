65a6cb1ba96fc31fa18a4563c5fc18d06abb4c82
doribasson <doribasson87@gmail.com>
2020-04-06 20:07:07 +0300

    Init commit4

---
+++ build/precache-manifest.767efb29ae0255de537f9ccbe6ab4aa3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea2e802c0f845d7e879bc3065a274907",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f05647999de27613a8c1",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "21d42b3ad5154947fbef",
+    "url": "/static/css/main.6159477b.chunk.css"
+  },
+  {
+    "revision": "f05647999de27613a8c1",
+    "url": "/static/js/2.3d248f77.chunk.js"
+  },
+  {
+    "revision": "28164e9e1f7b49ce7422bf240655ba5b",
+    "url": "/static/js/2.3d248f77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21d42b3ad5154947fbef",
+    "url": "/static/js/main.954dd3b0.chunk.js"
+  },
+  {
+    "revision": "5ebdaf550f852ac0b727",
+    "url": "/static/js/runtime-main.2fda9a68.js"
+  },
+  {
+    "revision": "39ac499c2a7856b24ffe3ffde4c8cc61",
+    "url": "/static/media/15-s.39ac499c.png"
+  },
+  {
+    "revision": "3c85da3e400a073c51b3ea73bdf5a130",
+    "url": "/static/media/17-s.3c85da3e.png"
+  },
+  {
+    "revision": "421c13f1b1eb0ae947bb64472fec6baa",
+    "url": "/static/media/41-s.421c13f1.png"
+  },
+  {
+    "revision": "4ea63ea2aa25e598de1f89d89957eb63",
+    "url": "/static/media/clouds.4ea63ea2.gif"
+  },
+  {
+    "revision": "7af8b23bf1396b9e3c97130dab36967f",
+    "url": "/static/media/search.7af8b23b.png"
+  }
+]);
